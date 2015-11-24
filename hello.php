6f732e9da6e6712dedae4b97000c1ab0d1e324e3
athinapos <ath.apostolopoulou@gmail.com>
2015-11-24 20:31:56 +0200

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,9 +1,6 @@
 <?php
 echo "Cloud: Lab II"\n;
-
-echo "AWS OpsWorks Services\n\n";
-
+echo "AWS OpsWorks Services\n";
 echo "Apostolopoulou Athina MPSP/14007\n";
-
 echo "Panagopoulou Maria MPSP/11013\n";
 ?>
